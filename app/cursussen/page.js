dea17a2a967666f9bdba0b7531605214fd3bace0
Brent Martens <brentmartens.info@gmail.com>
2026-04-11 17:33:56 +0200

    New release

--- app/cursussen/page.js
+++ app/cursussen/page.js
@@ -5,7 +5,7 @@ import Filters from '@/components/Filters';
 import ResultsBar from '@/components/ResultsBar';
 import CourseList from '@/components/CourseList';
 import Pagination from '@/components/Pagination';
-import { getAllCourses, getFilterOptions } from '@/lib/Courses';
+import { getAllCourses, getFilterOptions } from '@/lib/courses';
 
 /** Aantal cursussen per pagina */
 const PAGE_SIZE = 6;
