f7eeecfb1797c3e1a77139c6888d2772b6f7a3d7
Brent Martens <brentmartens.info@gmail.com>
2026-04-16 20:03:22 +0200

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
 
   return (
     <>
-      <header className="sticky top-0 z-50 bg-background border-b border-black/[0.08]">
+      <header className="md:sticky md:top-0 z-50 bg-background border-b border-black/[0.08]">
         <nav>
           <div className="flex items-center justify-between px-6 md:px-12 py-4">
 
